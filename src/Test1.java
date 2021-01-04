b483317fb9fbbf594af2274d173be1db5ba2dbc6
nithin <nithin.m.gowda2606@gmail.com>
2021-01-04 12:08:24 +0530

    First commit

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		String name = "nithin";
+		
+		if(name != null)
+			System.out.println("name is "+name);
+		else
+			System.out.println("no name");
+	}
+}
